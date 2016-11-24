f4174802536c9efe39dedf54c17d4a353c676bd7
Thomas H <thomashutchinson96@gmail.com>
2016-11-24 15:53:43 +0000

    File Setup

---
+++ csc2025-assignment2-init/do_getchildpids.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "pm.h"             // for glo.h:  mp, call_nr, who_p etc.
+#include "mproc.h"          // for proc table
+
+// system call for library function printchildpids
+int do_printchildpids() {
+
+    return -1;
+}
+
+// system call for library function getnchildren
+int do_getnchildren() {
+    
+    return -1;
+}
+
+// system call for library function getchildpids
+int do_getchildpids() {
+        
+    return -1;
+}
---
+++ csc2025-assignment2-init/getchildpids.c
@@ -0,0 +1,18 @@
+#include <lib.h>
+#include <unistd.h>
+#include <errno.h>
+
+int printchildpids(pid_t pid) {
+
+    return -1;
+}
+
+int getnchildren(pid_t pid) {
+    
+    return -1;
+}
+        
+int getchildpids(pid_t pid, int nchildren, pid_t *childpids) {
+    
+    return -1;
+}
